2c691b67b4b9a03ee3137ff7a80fabbdb0441a87
hekoh99 <hekoh99@naver.com>
2020-12-28 10:11:02 +0900

    done

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,103 @@
 import * as CryptoJS from "crypto-js";
 
-class Block{
-    public index:number;
-    public hash: string;
-    public previousHash : string;
-    public data : string;
-    public timestamp : number;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-    static calculateBlockHash = (
-        index:number, 
-        previousHash:string, 
-        timestamp:number, data:string
-        ):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
-    constructor(
-        index:number,
-        hash: string,
-        previousHash : string,
-        data : string,
-        timestamp : number){
-            this.index = index;
-            this.hash = hash;
-            this.previousHash = previousHash;
-            this.data = data;
-            this.timestamp = timestamp;
-        }
+  static validateStructure = (block: Block): boolean =>
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timestamp === "number" &&
+    typeof block.data === "string";
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const genesisBlock:Block = new Block(0, "example hash", "", "genBlock", 123456);
+const genesisBlock: Block = new Block(
+  0,
+  "example hash",
+  "",
+  "genBlock",
+  123456
+);
 
 let blockchain: Block[] = [genesisBlock];
 
-const getBlockchain = () : Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain;
 
-const getLatestBlock = () : Block => blockchain[blockchain.length -1];
-    
-const getNewTimestamp = () : number => Math.round(new Date().getTime() / 1000);
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const createNewBlock = (data:string) : Block => {
-    const previousBlock:Block = getLatestBlock();
-    const newIndex:number = previousBlock.index + 1;
-    const newTimestamp :number = getNewTimestamp();
-    const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
-    const newBlock:Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+const getHash = (block: Block): string =>
+  Block.calculateBlockHash(
+    block.index,
+    block.previousHash,
+    block.timestamp,
+    block.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) return false;
+  else if (previousBlock.index + 1 !== candidateBlock.index) return false;
+  else if (previousBlock.hash !== candidateBlock.previousHash) return false;
+  else if (getHash(candidateBlock) !== candidateBlock.hash) return false;
+  else return true;
+};
+
+const addBlock = (newBlock: Block): void => {
+  if (isBlockValid(newBlock, getLatestBlock())) {
     blockchain.push(newBlock);
+  }
+};
 
-    return newBlock;
-}
-createNewBlock("hello new blcok");
-createNewBlock("second creation");
+createNewBlock("first Block");
+createNewBlock("second block");
 console.log(blockchain);
 
 export {};
